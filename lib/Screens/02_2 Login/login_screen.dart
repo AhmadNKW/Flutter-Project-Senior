2053eede7bda8f2a1d6e20981f2887139d17d2ec
AhmadNKW <71257515+AhmadNKW@users.noreply.github.com>
2022-01-29 02:37:48 +0200

    sec

---
+++ lib/Screens/02_2 Login/login_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_auth/Screens/02_2%20Login/components/login_body.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: LoginBody(),
+    );
+  }
+}
